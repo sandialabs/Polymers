a98bc6e7d39a0a8f46b5e0ffa73f02e3121c9dde
mrbuche <mrbuche@sandia.gov>
2022-12-06 10:50:40 -0700

    setup for asymptotic relations for modified canonical ensemble

--- rust/src/physics/single_chain/fjc/thermodynamics/mod.rs
+++ rust/src/physics/single_chain/fjc/thermodynamics/mod.rs
@@ -136,5 +136,3 @@ pub trait ModifiedCanonicalAsymptoticWeakPotential
 {
     fn init(number_of_links: u8, link_length: f64, hinge_mass: f64) -> Self;
 }
-// really need test for prefactor... i.e. absolute free energy tests
-// and should make modifiedCanonical and isotensional have the extra mass term in absolute free energies (need to nondimensionalize partition functions) and make tests counting that
