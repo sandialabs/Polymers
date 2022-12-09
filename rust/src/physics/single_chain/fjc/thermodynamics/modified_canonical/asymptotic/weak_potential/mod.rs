5bbb048c42b61103535184049c1499954b976c52
mrbuche <mrbuche@sandia.gov>
2022-12-08 23:50:20 -0700

    similar issue (removable singularity) in weak potential asymptotic relation, removing relative measures

--- rust/src/physics/single_chain/fjc/thermodynamics/modified_canonical/asymptotic/weak_potential/mod.rs
+++ rust/src/physics/single_chain/fjc/thermodynamics/modified_canonical/asymptotic/weak_potential/mod.rs
@@ -63,38 +63,22 @@ impl ModifiedCanonicalAsymptoticWeakPotential for FJC
     }
     fn gibbs_free_energy(&self, potential_distance: &f64, potential_stiffness: &f64, temperature: &f64) -> f64
     {
-        self.relative_gibbs_free_energy(potential_distance, potential_stiffness, temperature) - self.number_of_links_f64*BOLTZMANN_CONSTANT*temperature*ln(&(8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)))
+        let nondimensional_force = potential_stiffness*potential_distance*self.link_length/BOLTZMANN_CONSTANT/temperature;
+        -self.number_of_links_f64*BOLTZMANN_CONSTANT*temperature*ln_sinhc(&nondimensional_force) + 0.5*potential_stiffness*self.contour_length.powf(2.0)*(langevin(&nondimensional_force).powf(2.0) + (nondimensional_force.powf(-2.0) - (nondimensional_force.sinh()).powf(-2.0))/self.number_of_links_f64) - self.number_of_links_f64*BOLTZMANN_CONSTANT*temperature*ln(&(8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)))
     }
     fn gibbs_free_energy_per_link(&self, potential_distance: &f64, potential_stiffness: &f64, temperature: &f64) -> f64
     {
-        self.relative_gibbs_free_energy_per_link(potential_distance, potential_stiffness, temperature) - BOLTZMANN_CONSTANT*temperature*ln(&(8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)))
-    }
-    fn relative_gibbs_free_energy(&self, potential_distance: &f64, potential_stiffness: &f64, temperature: &f64) -> f64
-    {
         let nondimensional_force = potential_stiffness*potential_distance*self.link_length/BOLTZMANN_CONSTANT/temperature;
-        -self.number_of_links_f64*BOLTZMANN_CONSTANT*temperature*ln_sinhc(&nondimensional_force) + 0.5*potential_stiffness*self.contour_length.powf(2.0)*(langevin(&nondimensional_force).powf(2.0) + (nondimensional_force.powf(-2.0) - (nondimensional_force.sinh()).powf(-2.0) - 1.0/3.0)/self.number_of_links_f64)
-    }
-    fn relative_gibbs_free_energy_per_link(&self, potential_distance: &f64, potential_stiffness: &f64, temperature: &f64) -> f64
-    {
-        let nondimensional_force = potential_stiffness*potential_distance*self.link_length/BOLTZMANN_CONSTANT/temperature;
-        -BOLTZMANN_CONSTANT*temperature*ln_sinhc(&nondimensional_force) + 0.5*potential_stiffness*self.contour_length.powf(2.0)*(langevin(&nondimensional_force).powf(2.0) + (nondimensional_force.powf(-2.0) - (nondimensional_force.sinh()).powf(-2.0) - 1.0/3.0)/self.number_of_links_f64)/self.number_of_links_f64
+        -BOLTZMANN_CONSTANT*temperature*ln_sinhc(&nondimensional_force) + 0.5*potential_stiffness*self.contour_length.powf(2.0)*(langevin(&nondimensional_force).powf(2.0) + (nondimensional_force.powf(-2.0) - (nondimensional_force.sinh()).powf(-2.0))/self.number_of_links_f64)/self.number_of_links_f64 - BOLTZMANN_CONSTANT*temperature*ln(&(8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)))
     }
     fn nondimensional_gibbs_free_energy(&self, nondimensional_potential_distance: &f64, nondimensional_potential_stiffness: &f64, temperature: &f64) -> f64
     {
-        self.nondimensional_relative_gibbs_free_energy(nondimensional_potential_distance, nondimensional_potential_stiffness) - self.number_of_links_f64*ln(&(8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)))
+        let nondimensional_force = nondimensional_potential_stiffness*nondimensional_potential_distance/self.number_of_links_f64;
+        -self.number_of_links_f64*ln_sinhc(&nondimensional_force) + 0.5*nondimensional_potential_stiffness*(langevin(&nondimensional_force).powf(2.0) + (nondimensional_force.powf(-2.0) - (nondimensional_force.sinh()).powf(-2.0))/self.number_of_links_f64) - self.number_of_links_f64*ln(&(8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)))
     }
     fn nondimensional_gibbs_free_energy_per_link(&self, nondimensional_potential_distance: &f64, nondimensional_potential_stiffness: &f64, temperature: &f64) -> f64
     {
-        self.nondimensional_relative_gibbs_free_energy_per_link(nondimensional_potential_distance, nondimensional_potential_stiffness) - ln(&(8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)))
-    }
-    fn nondimensional_relative_gibbs_free_energy(&self, nondimensional_potential_distance: &f64, nondimensional_potential_stiffness: &f64) -> f64
-    {
         let nondimensional_force = nondimensional_potential_stiffness*nondimensional_potential_distance/self.number_of_links_f64;
-        -self.number_of_links_f64*ln_sinhc(&nondimensional_force) + 0.5*nondimensional_potential_stiffness*(langevin(&nondimensional_force).powf(2.0) + (nondimensional_force.powf(-2.0) - (nondimensional_force.sinh()).powf(-2.0) - 1.0/3.0)/self.number_of_links_f64)
-    }
-    fn nondimensional_relative_gibbs_free_energy_per_link(&self, nondimensional_potential_distance: &f64, nondimensional_potential_stiffness: &f64) -> f64
-    {
-        let nondimensional_force = nondimensional_potential_stiffness*nondimensional_potential_distance/self.number_of_links_f64;
-        -ln_sinhc(&nondimensional_force) + 0.5*nondimensional_potential_stiffness*(langevin(&nondimensional_force).powf(2.0) + (nondimensional_force.powf(-2.0) - (nondimensional_force.sinh()).powf(-2.0) - 1.0/3.0)/self.number_of_links_f64)/self.number_of_links_f64
+        -ln_sinhc(&nondimensional_force) + 0.5*nondimensional_potential_stiffness*(langevin(&nondimensional_force).powf(2.0) + (nondimensional_force.powf(-2.0) - (nondimensional_force.sinh()).powf(-2.0))/self.number_of_links_f64)/self.number_of_links_f64 - ln(&(8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)))
     }
 }
