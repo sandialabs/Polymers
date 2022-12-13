cd19438ce951fdee703a1cdfbf53dc56d98a9f3f
mrbuche <dev@mrbuche.com>
2022-12-13 15:40:51 -0700

    reduced asymptotics efjc and basic tests

--- rust/src/physics/single_chain/efjc/thermodynamics/isotensional/asymptotic/reduced/legendre/mod.rs
+++ rust/src/physics/single_chain/efjc/thermodynamics/isotensional/asymptotic/reduced/legendre/mod.rs
@@ -30,4 +30,36 @@ impl Legendre for EFJC
             contour_length: (number_of_links as f64)*link_length,
         }
     }
+    fn helmholtz_free_energy(&self, force: &f64, temperature: &f64) -> f64
+    {
+        self.nondimensional_helmholtz_free_energy(&(force/BOLTZMANN_CONSTANT/temperature*self.link_length), temperature)*BOLTZMANN_CONSTANT*temperature
+    }
+    fn helmholtz_free_energy_per_link(&self, force: &f64, temperature: &f64) -> f64
+    {
+        self.nondimensional_helmholtz_free_energy_per_link(&(force/BOLTZMANN_CONSTANT/temperature*self.link_length), temperature)*BOLTZMANN_CONSTANT*temperature
+    }
+    fn relative_helmholtz_free_energy(&self, force: &f64, temperature: &f64) -> f64
+    {
+        self.helmholtz_free_energy(force, temperature) - self.helmholtz_free_energy(&(ZERO*BOLTZMANN_CONSTANT*temperature/self.link_length), temperature)
+    }
+    fn relative_helmholtz_free_energy_per_link(&self, force: &f64, temperature: &f64) -> f64
+    {
+        self.helmholtz_free_energy_per_link(force, temperature) - self.helmholtz_free_energy_per_link(&(ZERO*BOLTZMANN_CONSTANT*temperature/self.link_length), temperature)
+    }
+    fn nondimensional_helmholtz_free_energy(&self, nondimensional_force: &f64, temperature: &f64) -> f64
+    {
+        self.number_of_links_f64*self.nondimensional_helmholtz_free_energy_per_link(nondimensional_force, temperature)
+    }
+    fn nondimensional_helmholtz_free_energy_per_link(&self, nondimensional_force: &f64, temperature: &f64) -> f64
+    {
+        -(nondimensional_force.sinh()/nondimensional_force).ln() - 0.5*nondimensional_force.powf(2.0)/(self.link_stiffness*self.link_length.powf(2.0)/BOLTZMANN_CONSTANT/temperature) - 0.5*(2.0*PI*BOLTZMANN_CONSTANT*temperature/self.link_stiffness).ln() - (8.0*PI.powf(2.0)*self.hinge_mass*self.link_length.powf(2.0)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powf(2.0)).ln() + nondimensional_force/nondimensional_force.tanh() - 1.0 + nondimensional_force.powf(2.0)/(self.link_stiffness*self.link_length.powf(2.0)/BOLTZMANN_CONSTANT/temperature)
+    }
+    fn nondimensional_relative_helmholtz_free_energy(&self, nondimensional_force: &f64, temperature: &f64) -> f64
+    {
+        self.nondimensional_helmholtz_free_energy(nondimensional_force, temperature) - self.nondimensional_helmholtz_free_energy(&ZERO, temperature)
+    }
+    fn nondimensional_relative_helmholtz_free_energy_per_link(&self, nondimensional_force: &f64, temperature: &f64) -> f64
+    {
+        self.nondimensional_helmholtz_free_energy_per_link(nondimensional_force, temperature) - self.nondimensional_helmholtz_free_energy_per_link(&ZERO, temperature)
+    }
 }
