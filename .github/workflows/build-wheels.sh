1e33148224088112282588ca7d419b312151df80
mrbuche <dev@mrbuche.com>
2023-01-12 06:57:51 -0700

    trying more wheels, trouble with pip source distro

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 for PYBIN in /opt/python/cp3[67891]*/bin; do
-    "${PYBIN}/pip" install maturin
+    "${PYBIN}/pip" install cffi maturin
     "${PYBIN}/maturin" build -i "${PYBIN}/python" --release
 done
 
