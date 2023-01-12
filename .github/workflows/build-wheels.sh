10087bf3d8af3cfaff9cb666ea1cbd076546bf4e
mrbuche <dev@mrbuche.com>
2023-01-12 08:11:51 -0700

    moving conda

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -3,7 +3,7 @@ set -e -x
 
 for PYBIN in /opt/python/cp3[67891]*/bin; do
     "${PYBIN}/pip" install cffi maturin
-    "${PYBIN}/maturin" build -i "${PYBIN}/python" --release --features python
+    "${PYBIN}/maturin" build --features python -i "${PYBIN}/python" --release
 done
 
 for wheel in target/wheels/*.whl; do
