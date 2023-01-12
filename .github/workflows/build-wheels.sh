6d9953f761ef8535a04c0a450d467004b9fc1c5b
mrbuche <dev@mrbuche.com>
2023-01-12 08:05:24 -0700

    forgot features

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -3,7 +3,7 @@ set -e -x
 
 for PYBIN in /opt/python/cp3[67891]*/bin; do
     "${PYBIN}/pip" install cffi maturin
-    "${PYBIN}/maturin" build -i "${PYBIN}/python" --release
+    "${PYBIN}/maturin" build -i "${PYBIN}/python" --release --features python
 done
 
 for wheel in target/wheels/*.whl; do
