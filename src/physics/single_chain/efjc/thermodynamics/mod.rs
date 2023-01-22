0a43d67bcbcaa94062dc44532c6e4de4c1d1a831
mrbuche <dev@mrbuche.com>
2023-01-21 23:33:55 -0700

    started on efjc python/rust api/docs

--- src/physics/single_chain/efjc/thermodynamics/mod.rs
+++ src/physics/single_chain/efjc/thermodynamics/mod.rs
@@ -1,15 +1,34 @@
+#[cfg(feature = "python")]
+pub mod py;
+
 mod test;
+
+/// The extensible freely-jointed chain (EFJC) model thermodynamics in the isotensional ensemble.
 pub mod isotensional;
-pub struct EFJC
+
+/// The structure of the thermodynamics of the FJC model.
+pub struct FJC
 {
+    /// The mass of each hinge in the chain in units of kg/mol.
     pub hinge_mass: f64,
+
+    /// The length of each link in the chain in units of nm.
     pub link_length: f64,
+
+    /// The number of links in the chain.
     pub number_of_links: u8,
+
+    /// The stiffness of each link in the chain in units of J/(mol⋅nm^2).
     pub link_stiffness: f64,
-    pub isotensional: self::isotensional::EFJC,
+
+    /// The thermodynamic functions of the model in the isotensional ensemble.
+    pub isotensional: isotensional::FJC
 }
+
+/// The implemented functionality of the thermodynamics of the EFJC model.
 impl EFJC
 {
+    /// Initializes and returns an instance of the thermodynamics of the EFJC model.
     pub fn init(number_of_links: u8, link_length: f64, hinge_mass: f64, link_stiffness: f64) -> Self
     {
         EFJC
--- src/physics/single_chain/py.rs
+++ src/physics/single_chain/py.rs
@@ -5,7 +5,7 @@ pub fn register_module(py: Python<'_>, parent_module: &PyModule) -> PyResult<()>
     let single_chain = PyModule::new(py, "single_chain")?;
     super::ideal::py::register_module(py, single_chain)?;
     super::fjc::py::register_module(py, single_chain)?;
-    // super::efjc::py::register_module(py, single_chain)?;
+    super::efjc::py::register_module(py, single_chain)?;
     super::swfjc::py::register_module(py, single_chain)?;
     parent_module.add_submodule(single_chain)?;
     Ok(())
