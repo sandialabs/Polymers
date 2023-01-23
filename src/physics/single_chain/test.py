0517e438e541c3cb8714d88856930bba1e46646d
mrbuche <dev@mrbuche.com>
2023-01-22 17:09:17 -0700

    will duplicate tests for python/pytest, also pycodestyle/pylint applied in checks

---
+++ src/physics/single_chain/test.py
@@ -0,0 +1,25 @@
+"""Module to test ...
+
+"""
+
+import unittest
+
+from polymers import physics
+single_chain = physics.single_chain
+
+
+class Class(unittest.TestCase):
+    """Class to test ...
+
+    """
+    def test_something(self):
+        """Function to test ...
+
+        """
+        self.assertEqual(8, 8)
+
+    def test_something_else(self):
+        """Function to test ...
+
+        """
+        self.assertEqual(8, 8)
