c1a47e17f6a8da3c59424cd0ff6aa617372cc305
mrbuche <dev@mrbuche.com>
2023-01-22 22:11:50 -0700

    basic pytest config

---
+++ src/physics/single_chain/fjc/thermodynamics/isotensional/test.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+
+
+class Parameters:
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+        self.number_of_links_minimum = 5
+        self.link_length_reference = 1.0
+        self.hinge_mass_reference = 1.0
