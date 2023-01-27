c162ebc3ebc634ca896e0f8651ab9c1265714c78
mrbuche <dev@mrbuche.com>
2023-01-27 10:46:48 -0700

    will force all tests to be ready on all languages

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--my_test_dump", action="store", default=None,
+    parser.addoption("--compare", action="store", default=None,
         help="Print test items in my custom format")
 
 def pytest_collection_finish(session):
-    if session.config.option.my_test_dump is not None:
+    if session.config.option.compare is not None:
         for item in session.items:
             print('{}::{}'.format(item.fspath, item.name))
         pytest.exit('Done!')
