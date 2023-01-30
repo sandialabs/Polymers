273737aa930b1e5fcacb08df096d5e0913231f2d
mrbuche <mrbuche@sandia.gov>
2023-01-30 15:28:05 -0700

    normalization tests

--- src/physics/single_chain/test.py
+++ src/physics/single_chain/test.py
@@ -1366,7 +1366,7 @@ class FjcIdeal(unittest.TestCase):
                 number_of_links*link_length
             equilibrium_distribution_fjc = \
                 fjc.thermodynamics.isometric. \
-                equilibrium_distribution(
+                equilibrium_radial_distribution(
                     np.array(end_to_end_length)
                 )
             equilibrium_distribution_ideal = \
