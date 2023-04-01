e78e244bb14fa461c0463f4c8113be35864d0f31
mrbuche <mrbuche@sandia.gov>
2023-04-01 15:53:52 -0600

    take reimpl of integrate out of test

--- src/physics/single_chain/test.rs
+++ src/physics/single_chain/test.rs
@@ -6,12 +6,6 @@ use super::{
     swfjc::SWFJC
 };
 use crate::physics::BOLTZMANN_CONSTANT;
-pub fn integrate<F>(function: F, lower_lim: &f64, upper_lim: &f64, num_points: &u128) -> f64
-where F: Fn(f64) -> f64
-{
-    let dx = (upper_lim - lower_lim)/(*num_points as f64);
-    (0..=num_points-1).collect::<Vec::<u128>>().iter().map(|index| function(lower_lim + (0.5 + *index as f64)*dx)).sum::<f64>()*dx
-}
 pub struct Parameters
 {
     pub abs_tol: f64,
