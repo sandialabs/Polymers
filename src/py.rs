a43033c415c502d707571605d2b75d4b450d6efa
mrbuche <mrbuche@sandia.gov>
2023-04-02 14:08:25 -0600

    how to pass python function to pyo3 function?

--- src/py.rs
+++ src/py.rs
@@ -2,6 +2,7 @@ use pyo3::prelude::*;
 
 #[pymodule]
 pub fn polymers(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    super::math::py::register_module(py, m)?;
     super::physics::py::register_module(py, m)?;
     Ok(())
 }
