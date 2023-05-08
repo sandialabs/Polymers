b85b87f2bddf27c8c8e169962673b1ef784bb737
mrbuche <dev@mrbuche.com>
2023-05-08 08:04:44 -0600

    temporary surgery

--- src/physics/single_chain/wlc/thermodynamics/isotensional/mod.rs
+++ src/physics/single_chain/wlc/thermodynamics/isotensional/mod.rs
@@ -10,7 +10,11 @@ mod test;
 pub mod legendre;
 
 use std::f64::consts::PI;
-use crate::math::integrate_1d;
+use crate::math::
+{
+    inverse_newton_raphson_powered,
+    integrate_1d
+};
 use crate::physics::
 {
     PLANCK_CONSTANT,
@@ -22,6 +26,7 @@ use crate::physics::single_chain::
     ZERO,
     POINTS
 };
+use super::isometric::nondimensional_force as isometric_nondimensional_force;
 use super::isometric::nondimensional_helmholtz_free_energy as isometric_nondimensional_helmholtz_free_energy;
 use super::isometric::nondimensional_relative_helmholtz_free_energy as isometric_nondimensional_relative_helmholtz_free_energy;
 
@@ -112,13 +117,46 @@ pub fn relative_gibbs_free_energy_per_link(number_of_links: &u8, link_length: &f
 /// The nondimensional Gibbs free energy as a function of the applied nondimensional force and temperature, parameterized by the number of links, link length, hinge mass, and nondimensional persistance length.
 pub fn nondimensional_gibbs_free_energy(number_of_links: &u8, link_length: &f64, hinge_mass: &f64, nondimensional_persistance_length: &f64, nondimensional_force: &f64, temperature: &f64) -> f64
 {
-    let eta_n = (*number_of_links as f64)*nondimensional_force;
+    // extremize N*(eta*gamma - vartheta)
+    // d/d_gamma =0 => eta - eta_isometric(gamma) = 0
+    // extremized at eta = eta_isometric(gamma_hat)
+    // d^2/d_gamma^2 => -eta_isometric' => eta_isometric totally monotone(?) => extremum is a maximum, as desired
+    // get gamma_hat from root of f(gamma) = eta - eta_isometric(gamma)
+    // maybe set eta_n based on this, when eta is above some value, or when exp() overflows?
+    let f = &|nondimensional_end_to_end_length_per_link: &f64| isometric_nondimensional_force(number_of_links, nondimensional_persistance_length, nondimensional_end_to_end_length_per_link);
+    let h = 1e-8;
+    let fp = &|nondimensional_end_to_end_length_per_link: &f64| (isometric_nondimensional_force(number_of_links, nondimensional_persistance_length, &(nondimensional_end_to_end_length_per_link + h)) - isometric_nondimensional_force(number_of_links, nondimensional_persistance_length, &(nondimensional_end_to_end_length_per_link - h)))/2.0/h;
+    let fpp = &|x: &f64| (fp(&(x + h)) - fp(&(x - h)))/2.0/h;
+    let gamma_hat = inverse_newton_raphson_powered(
+        nondimensional_force,
+        f,
+        fp,
+        &0.9999,
+        &1e-5,
+        &100,
+        2
+    );
+    println!("{:?}", gamma_hat);
+    if gamma_hat > 1.0 || gamma_hat < 0.0
+    {
+        panic!("Root finding failed!");
+    }
+    let eta_n = (*number_of_links as f64)*nondimensional_force*gamma_hat - isometric_nondimensional_helmholtz_free_energy(number_of_links, link_length, hinge_mass, nondimensional_persistance_length, &gamma_hat, temperature);
+    if eta_n.is_infinite()
+    {
+        panic!("test");
+    }
     let integrand = |nondimensional_end_to_end_length_per_link: &f64|
     {
         let beta_psi = isometric_nondimensional_helmholtz_free_energy(number_of_links, link_length, hinge_mass, nondimensional_persistance_length, nondimensional_end_to_end_length_per_link, temperature);
         let beta_f_xi = (*number_of_links as f64)*nondimensional_force*nondimensional_end_to_end_length_per_link;
+//        println!("{:?}", (beta_psi, beta_f_xi, eta_n, (beta_f_xi - eta_n - beta_psi).exp(), (-beta_f_xi - eta_n - beta_psi).exp()));
         0.5*((beta_f_xi - eta_n - beta_psi).exp() - (-beta_f_xi - eta_n - beta_psi).exp())*(*number_of_links as f64)*nondimensional_end_to_end_length_per_link/nondimensional_force
     };
+    println!("{:?}", (
+        -(integrate_1d(&integrand, &ZERO, &ONE, &POINTS)).ln(),
+        -((2.0*PI/fpp(&gamma_hat)).sqrt()).ln() - (0.5*(*number_of_links as f64)*gamma_hat/nondimensional_force).ln()
+    ));
     -(4.0*PI*integrate_1d(&integrand, &ZERO, &ONE, &POINTS)).ln() - eta_n - (4.0*(-1.0/nondimensional_persistance_length).exp().acos().sin()*PI.powi(2)*hinge_mass*link_length.powi(2)*BOLTZMANN_CONSTANT*temperature/PLANCK_CONSTANT.powi(2)).ln()
 }
 
