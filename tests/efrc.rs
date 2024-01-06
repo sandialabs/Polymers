67f29fedfdc5bf55716a0c1aabd73deb417a0ed4
Michael R. Buche <mrbuche@sandia.gov>
2024-01-06 14:00:33 -0700

    more data

--- tests/efrc.rs
+++ tests/efrc.rs
@@ -18,9 +18,9 @@ const TOL: f64 = 5e-2;
 #[test]
 fn temporary()
 {
-    let (gamma, g_eq) = nondimensional_equilibrium_radial_distribution::<1_000, 8>(&1.3, &25.0, &(PI/4.0), 1_000_000_000);
-    gamma.iter().zip(g_eq.iter()).for_each(|output|
-        println!("{:?}", output)
+    let (gamma, g_eq) = nondimensional_equilibrium_radial_distribution::<1_000, 8>(&1.6, &10.0, &(PI/4.0), 5_000_000_000);
+    gamma.iter().zip(g_eq.iter()).for_each(|(gamma, g_eq)|
+        println!("{}\t{}", gamma, g_eq)
     );
 }
 
