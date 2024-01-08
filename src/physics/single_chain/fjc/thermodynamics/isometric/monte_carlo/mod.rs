4be93091ccc533321bada2958e2c8c9cf8ad4519
mrbuche <dev@mrbuche.com>
2024-01-08 08:14:21 -0700

    new idea

--- src/physics/single_chain/fjc/thermodynamics/isometric/monte_carlo/mod.rs
+++ src/physics/single_chain/fjc/thermodynamics/isometric/monte_carlo/mod.rs
@@ -23,13 +23,47 @@ pub fn random_configuration<const NUMBER_OF_LINKS: usize>(rng: &mut ThreadRng) -
     configuration
 }
 
+pub fn random_configuration_with_cos_angles<const NUMBER_OF_LINKS: usize>(rng: &mut ThreadRng) -> ([[f64; 3]; NUMBER_OF_LINKS], [f64; NUMBER_OF_LINKS])
+{
+    let mut phi: f64 = 0.0;
+    let mut theta: f64 = 0.0;
+    let mut position = [0.0; 3];
+    let mut configuration = [[0.0; 3]; NUMBER_OF_LINKS];
+    let mut cos_angles = [0.0; NUMBER_OF_LINKS];
+    configuration.iter_mut().zip(cos_angles.iter_mut()).for_each(|(coordinate, cos_angle)|{
+        phi = TWO_PI * rng.gen::<f64>();
+        theta = (1.0 - 2.0 * rng.gen::<f64>()).acos();
+        position[0] += theta.sin() * phi.cos();
+        position[1] += theta.sin() * phi.sin();
+        position[2] += theta.cos();
+        *cos_angle = position[2];
+        coordinate.iter_mut().zip(position.iter()).for_each(|(coordinate_i, position_i)|
+            *coordinate_i = *position_i
+        );
+    });
+    (configuration, cos_angles)
+}
+
 pub fn random_nondimensional_end_to_end_length<const NUMBER_OF_LINKS: usize>(rng: &mut ThreadRng) -> f64
 {
     random_configuration::<NUMBER_OF_LINKS>(rng)[NUMBER_OF_LINKS - 1].iter().map(|entry| entry * entry).sum::<f64>().sqrt()
 }
 
+pub fn random_nondimensional_end_to_end_length_with_cos_angles<const NUMBER_OF_LINKS: usize>(rng: &mut ThreadRng) -> (f64, [f64; NUMBER_OF_LINKS])
+{
+    let (configuration, cos_angles) = random_configuration_with_cos_angles::<NUMBER_OF_LINKS>(rng);
+    let xi = configuration[NUMBER_OF_LINKS - 1].iter().map(|entry| entry * entry).sum::<f64>().sqrt();
+    (xi, cos_angles)
+}
+
 pub fn nondimensional_equilibrium_radial_distribution<const NUMBER_OF_BINS: usize, const NUMBER_OF_LINKS: usize>(number_of_samples: usize) -> ([f64; NUMBER_OF_BINS], [f64; NUMBER_OF_BINS])
 {
+    //
+    // try to pull parts of this out and minimize code duplication
+    // should try to do similar above for random configs
+    //
+    // need to calculate running average of 3 moments for each link in each bin
+    //
     let mut rng = rand::thread_rng();
     let number_of_links_f64 = NUMBER_OF_LINKS as f64;
     let mut bin_edges = [0.0_f64; NUMBER_OF_BINS];
