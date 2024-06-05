71f4ffc04ece109e8b86022deee69134a96a15d8
mrbuche <dev@mrbuche.com>
2024-06-05 11:32:19 -0600

    move to an example

--- examples/efrc.rs
+++ examples/efrc.rs
@@ -9,7 +9,7 @@ use std::
 
 fn main()
 {
-    let (gamma, g_eq) = nondimensional_equilibrium_radial_distribution::<1_000, 8>(&1.2, &50.0, &(PI/4.0), 1_000_000);
+    let (gamma, g_eq) = nondimensional_equilibrium_radial_distribution::<1_000, 8>(&1.25, &50.0, &(PI/4.0), 1_000_000);
     let mut file = File::create("examples/efrc.csv").unwrap();
     gamma.iter().zip(g_eq.iter()).for_each(|(gamma, g_eq)|{
         writeln!(&mut file, "{}\t{}", gamma, g_eq).unwrap();
