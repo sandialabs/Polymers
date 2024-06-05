64a0f9f82f24c42eac35ef3a22afbfe183a86bca
mrbuche <dev@mrbuche.com>
2024-06-05 12:05:37 -0600

    multithread

--- examples/efrc.rs
+++ examples/efrc.rs
@@ -4,14 +4,26 @@ use std::
 {
     f64::consts::PI,
     fs::File,
-    io::Write
+    io::Write,
+    thread
 };
 
-fn main()
+fn compute(index: usize)
 {
     let (gamma, g_eq) = nondimensional_equilibrium_radial_distribution::<1_000, 8>(&1.25, &50.0, &(PI/4.0), 1_000_000);
-    let mut file = File::create("examples/efrc.csv").unwrap();
+    let file_name = format!("examples/efrc_{}.csv", index);
+    let mut file = File::create(file_name).unwrap();
     gamma.iter().zip(g_eq.iter()).for_each(|(gamma, g_eq)|{
         writeln!(&mut file, "{}\t{}", gamma, g_eq).unwrap();
     });
+}
+
+fn main() {
+    let handle_1 = thread::spawn(|| compute(1));
+    let handle_2 = thread::spawn(|| compute(2));
+    let handle_3 = thread::spawn(|| compute(3));
+    compute(0);
+    handle_1.join().unwrap();
+    handle_2.join().unwrap();
+    handle_3.join().unwrap();
 }
