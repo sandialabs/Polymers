77999f8a9b5dd0d7160aeccc1e664ea4f484520b
mrbuche <dev@mrbuche.com>
2024-06-05 13:10:18 -0600

    option to change directory

--- examples/efrc.rs
+++ examples/efrc.rs
@@ -2,6 +2,7 @@ use polymers::physics::single_chain::efrc::thermodynamics::isometric::monte_carl
 
 use std::
 {
+    env,
     f64::consts::PI,
     fs::File,
     io::Write,
@@ -10,8 +11,9 @@ use std::
 
 fn compute(index: usize)
 {
+    let file_path = &env::args().collect::<Vec<String>>()[1];
     let (gamma, g_eq) = nondimensional_equilibrium_radial_distribution::<1_000, 8>(&1.25, &50.0, &(PI/4.0), 1_000_000);
-    let file_name = format!("examples/efrc_{}.csv", index);
+    let mut file_name = file_path.to_owned() + &format!("/efrc_{}.csv", index);
     let mut file = File::create(file_name).unwrap();
     gamma.iter().zip(g_eq.iter()).for_each(|(gamma, g_eq)|{
         writeln!(&mut file, "{}\t{}", gamma, g_eq).unwrap();
